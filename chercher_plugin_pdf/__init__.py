b76b90ccbd820faa8b15fb4f6f7b4985e6c97c9f
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2025-05-03 10:15:17 +0200

    feat: add prune hook

--- chercher_plugin_pdf/__init__.py
+++ chercher_plugin_pdf/__init__.py
@@ -24,11 +24,29 @@ def ingest(uri: str) -> Generator[Document, None, None]:
 
     body = ""
     with pymupdf.open(path) as doc:
+        metadata = doc.metadata
         for page in doc:
             body += page.get_text()
 
     yield Document(
         uri=path.as_uri(),
+        title=metadata.get("title", ""),
         body=body,
-        metadata={},
+        metadata=metadata,
     )
+
+
+@hookimpl()
+def prune(uri: str) -> bool | None:
+    if uri.startswith("file://"):
+        parsed_uri = urlparse(uri)
+        decoded_path = unquote(parsed_uri.path)
+        path = Path(decoded_path).resolve()
+    else:
+        return
+
+    if path.suffix != ".pdf":
+        return
+
+    if not path.exists() or not path.is_file():
+        return True
--- tests/test_chercher_plugin_pdf.py
+++ tests/test_chercher_plugin_pdf.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import pytest
-from chercher_plugin_pdf import ingest
+from chercher_plugin_pdf import ingest, prune
 from chercher import Document
 
 
@@ -10,17 +10,19 @@ def sample_files():
     return [file.as_uri() for file in samples_dir.iterdir() if file.is_file()]
 
 
-def test_valid_file(sample_files):
+def test_ingest_valid_file(sample_files):
     for uri in sample_files:
         documents = list(ingest(uri=uri))
         assert documents != []
         for doc in documents:
             assert isinstance(doc, Document)
             assert doc.uri == uri
+            assert doc.title != ""
             assert doc.body != ""
+            assert isinstance(doc.metadata, dict)
 
 
-def test_invalid_file(tmp_path):
+def test_ingest_invalid_file(tmp_path):
     p = tmp_path / "test.txt"
     p.write_text("Test")
 
@@ -29,13 +31,29 @@ def test_invalid_file(tmp_path):
     assert documents == []
 
 
-def test_missing_file(tmp_path):
+def test_ingest_missing_file(tmp_path):
     p = tmp_path / "missingno.epub"
     documents = list(ingest(uri=p.as_uri()))
     assert documents == []
 
 
-def test_invalid_uri():
+def test_ingest_invalid_uri():
     uri = "https://www.gutenberg.org/cache/epub/11/pg11-images.html"
     documents = list(ingest(uri=uri))
     assert documents == []
+
+
+def test_prune_valid_file(sample_files):
+    uri = sample_files[0]
+    assert prune(uri=uri) is None
+
+
+def test_prune_missing_file(tmp_path):
+    p = tmp_path / "missingno.pdf"
+    uri = p.as_uri()
+    assert prune(uri=uri)
+
+
+def test_prune_invalid_uri():
+    uri = "https://files/file.pdf"
+    assert prune(uri=uri) is None
